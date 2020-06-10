d08f34ac9d122c7c1652d4dfd0fcf4db11132eee
JMIsham <isham.mohamed@syscolabs.com>
2020-06-10 22:55:07 +0530

    implement all 3 wuestions

--- code/main.cpp
+++ code/main.cpp
@@ -11,6 +11,13 @@
 using namespace std;
 using namespace std::chrono;
 
+void printTime(std::chrono::microseconds duration, size_t height,  string tree, string data) {
+    cout << "\nTime taken " << data << " " << tree <<": "
+         << duration.count() << " microseconds" << endl;
+    cout << "Resulting tree height " << data << " " << tree << ": "
+         << height << endl;
+}
+
 int main() {
     //loading all data in memory to avoud IO time included in the tree opetations
     vector<int> data_1;
@@ -55,6 +62,9 @@ int main() {
 	in_search_data.close();
 
 
+    //Question 1
+
+    cout << "\n=============Question 1==============="<< endl;
 
     auto start = high_resolution_clock::now(); 
     auto bst_1 = BST();
@@ -65,8 +75,7 @@ int main() {
     
     auto stop = high_resolution_clock::now(); 
     auto duration = duration_cast<microseconds>(stop - start); 
-    cout << "Time taken data_1 BST: "
-         << duration.count() << " microseconds" << endl;
+    printTime(duration, bst_1.height(), "BST", "data_1");
 
 
     start = high_resolution_clock::now(); 
@@ -78,8 +87,197 @@ int main() {
     
     stop = high_resolution_clock::now(); 
     duration = duration_cast<microseconds>(stop - start); 
-    cout << "Time taken data_2 BST: "
-         << duration.count() << " microseconds" << endl;
+    printTime(duration, bst_2.height(), "BST", "data_2");
+
+    
+    start = high_resolution_clock::now(); 
+    auto splay_1 = SplayTree();
+    auto splay_new_1 = SplayTree(); //since search changes the tree, this will be used for deletion
+    for (int num : data_1)
+        {
+            splay_1.put(num, num);
+            splay_new_1.put(num, num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, splay_1.height(), "Spaly Tree", "data_1");
+
+
+    start = high_resolution_clock::now(); 
+    auto splay_2 = SplayTree();
+    auto splay_new_2 = SplayTree(); //since search changes the tree, this will be used for deletion
+    for (int num : data_2)
+        {
+            splay_2.put(num, num);
+            splay_new_2.put(num, num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, splay_2.height(), "Spaly Tree", "data_2");
+
+    
+    start = high_resolution_clock::now(); 
+    auto RB_1 = RBTree();
+    for (int num : data_1)
+        {
+            RB_1.put(num, num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, RB_1.height(), "RB-Tree", "data_1");
+
+    start = high_resolution_clock::now(); 
+    auto RB_2 = RBTree();
+    for (int num : data_2)
+        {
+            RB_2.put(num, num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, RB_2.height(), "RB-Tree", "data_2");
+
+
+    //QUESTION 2
+
+    cout << "\n=============Question 2==============="<< endl;
+    start = high_resolution_clock::now(); 
+    for (int num : search_data)
+        {
+            bst_1.contains(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, bst_1.height(), "BST", "data_1");
+
+
+    start = high_resolution_clock::now(); 
+    for (int num : search_data)
+        {
+            bst_2.contains(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, bst_2.height(), "BST", "data_2");
+
+    
+    start = high_resolution_clock::now(); 
+    for (int num : search_data)
+        {
+            splay_1.contains(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, splay_1.height(), "Spaly Tree", "data_1");
+
+        
+    start = high_resolution_clock::now(); 
+    for (int num : search_data)
+        {
+            splay_2.contains(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, splay_2.height(), "Spaly Tree", "data_2");
+
+    start = high_resolution_clock::now(); 
+    for (int num : search_data)
+        {
+            RB_1.contains(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, RB_1.height(), "RB Tree", "data_1");
+
+
+    start = high_resolution_clock::now(); 
+    for (int num : search_data)
+        {
+            RB_2.contains(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, RB_2.height(), "RB Tree", "data_2");
+        
+    
+
+    //Question 3
+
+    cout << "\n=============Question 3==============="<< endl;
+
+    start = high_resolution_clock::now(); 
+    for (int num : delete_data)
+        {
+            bst_1.del(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, bst_1.height(), "BST", "data_1");
+
+
+    start = high_resolution_clock::now(); 
+    for (int num : delete_data)
+        {
+            bst_2.del(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, bst_2.height(), "BST", "data_2");
+
+
+    start = high_resolution_clock::now(); 
+    for (int num : delete_data)
+        {
+            splay_new_1.del(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, splay_new_1.height(), "Spaly Tree", "data_1");
+
+        
+    start = high_resolution_clock::now(); 
+    for (int num : delete_data)
+        {
+            splay_new_2.del(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, splay_new_2.height(), "Spaly Tree", "data_2");
+
+
+    start = high_resolution_clock::now(); 
+    for (int num : delete_data)
+        {
+            RB_1.del(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, RB_1.height(), "RB Tree", "data_1");
+
+
+    start = high_resolution_clock::now(); 
+    for (int num : delete_data)
+        {
+            RB_2.del(num);
+        }
+    
+    stop = high_resolution_clock::now(); 
+    duration = duration_cast<microseconds>(stop - start); 
+    printTime(duration, RB_2.height(), "RB Tree", "data_2");
 
     
     return 0;
